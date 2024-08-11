8067777935cd8b7ffb04dfa146aa9a20ebb95c3a
chuva <chuvachoc2009@yandex.ru>
2024-08-11 19:58:45 +0300

    Ищем числа Фибоначчи

---
+++ .venv/fibonachi_numbers.py
@@ -0,0 +1,23 @@
+
+
+def fibonacci_set(number):
+    """Генерирует набор чисел Фибоначчи вплоть до заданного числа.
+
+    Args:
+        number: Верхний предел для чисел Фибоначчи
+
+    Returns:
+        Набор, содержащий числа Фибоначчи, меньшие или равные "number".
+    """
+    fib_set = {0, 1}  # Инициализация первых двух чисел Фибоначчи,
+    a, b = 0, 1
+    while b <= number:
+        fib_set.add(b)
+        a, b = b, a + b
+    return fib_set
+
+number = int(input())
+result = tuple(fibonacci_set(number))
+result = sorted(result)
+print(result)
+
