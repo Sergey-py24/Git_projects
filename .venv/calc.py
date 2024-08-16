94396a47a54330a3a1de85d04ad2ce1393a646da
chuva <chuvachoc2009@yandex.ru>
2024-08-16 22:59:17 +0300

    добавил проценты

--- .venv/calc.py
+++ .venv/calc.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 
 
+
 def get_values():
     num1 = int(number1_entry.get())
     num2 = int(number2_entry.get())
@@ -36,29 +37,38 @@ def mul():
     insert_values(res)
 
 
+
+def per():
+    num1, num2 = get_values()
+    res = (num2 / num1) * 100
+    insert_values(res)
+
+
 window = tk.Tk()
 window.title('Счетная машина')
-window.geometry("350x350")
+window.geometry("400x400")
 window.resizable(False, False)
-button_add = tk.Button(window, text="+", width=2, height=2, command=add,bg='blue')
+button_add = tk.Button(window, text="+", width=5, height=2, command=add,bg='blue')
 button_add.place(x=100, y=200)
-button_sub = tk.Button(window, text="-", width=2, height=2, command=sub, bg='blue')
+button_sub = tk.Button(window, text="-", width=5, height=2, command=sub, bg='blue')
 button_sub.place(x=150, y=200)
-button_mul = tk.Button(window, text="*", width=2, height=2, command=mul, bg='blue')
+button_mul = tk.Button(window, text="*", width=5, height=2, command=mul, bg='blue')
 button_mul.place(x=200, y=200)
-button_div = tk.Button(window, text="/", width=2, height=2, command=div, bg='blue')
+button_div = tk.Button(window, text="/", width=5, height=2, command=div, bg='blue')
 button_div.place(x=250, y=200)
-number1_entry = tk.Entry(window, width=28)
-number1_entry.place(x=100, y=75)
-number2_entry = tk.Entry(window, width=28)
-number2_entry.place(x=100, y=150)
-answer_entry = tk.Entry(window, width=28)
-answer_entry.place(x=100, y=300)
-number1 = tk.Label(window, text="Введите первое число:", font=("Helvetica", "10", "italic"))
+button_per = tk.Button(window, text='%', width=5, height=2, command=per, bg='blue')
+button_per.place(x=300, y=200)
+number1_entry = tk.Entry(window, width=32)
+number1_entry.place(x=100, y=90)
+number2_entry = tk.Entry(window, width=32)
+number2_entry.place(x=100, y=165)
+answer_entry = tk.Entry(window, width=32)
+answer_entry.place(x=100, y=315)
+number1 = tk.Label(window, text="Введите первое число:", font=("Helvetica", "13", "italic"))
 number1.place(x=100, y=50)
-number2 = tk.Label(window, text="Введите второе число:", font=("Helvetica", "10", "italic"))
+number2 = tk.Label(window, text="Введите второе число:", font=("Helvetica", "13", "italic"))
 number2.place(x=100, y=125)
-answer = tk.Label(window, text="Результат:", font=("Helvetica", "10", "italic"))
+answer = tk.Label(window, text="Результат:", font=("Helvetica", "13", "italic"))
 answer.place(x=100, y=275)
 window.mainloop()
 
