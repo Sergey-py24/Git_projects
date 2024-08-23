13c7416e538e804fd8ad168aecd35c8d5edc3848
chuva <chuvachoc2009@yandex.ru>
2024-08-23 17:04:47 +0300

    sort 3 options

--- .venv/sortfunc.py
+++ .venv/sortfunc.py
@@ -1,7 +1,7 @@
-nums = [3, 2, 4, 9, 7]
+nums = [33, 22, 44, 99, 77, 88, 66, 55]
 
 
-def bubble_sort(ls):  #пузырьковая сортировка
+def bubble_sort(ls):  # пузырьковая сортировка
     swapped = True
     while swapped:
         swapped = False
@@ -15,13 +15,28 @@ def bubble_sort(ls):  #пузырьковая сортировка
 # print(nums)
 
 
-def selection_sort(ls):  #сортировка выборкой
+def selection_sort(ls):  # сортировка выборкой
     for i in range(len(ls)):
         lowest = i
         for j in range(i + 1, len(ls)):
             if ls[j] < ls[lowest]:
                 lowest = j
         ls[i], ls[lowest] = ls[lowest], ls[i]
+
+
+# selection_sort(nums)
+# print(nums)
+
+
+def insertion_sort(ls):
+    for i in range(1, len(ls)):
+        key = ls[i]
+        j = i - 1
+        while ls[j] > key and j >= 0:
+            ls[j + 1] = ls[j]
+            j = 1
+        ls[j + 1] = key
+    return ls
 
 
 selection_sort(nums)
